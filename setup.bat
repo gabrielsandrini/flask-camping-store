79856196b7491e88e90301e7afc04c5a02d1f3ef
Letícia Fernandes <lemayara16@gmail.com>
2021-05-27 21:08:03 -0300

    criação venv e gitignore

---
+++ setup.bat
@@ -0,0 +1,13 @@
+ pip install --upgrade pip  
+ pip install Flask
+ pip install Flask-SQLAlchemy
+ pip install Flask-Migrate
+ pip install Flask-Script
+ pip install passlib
+ pip install Flask-admin
+ pip install Flask-Bootstrap4
+ pip install pyjwt
+ pip install Flask-login
+ pip install sendgrid
+ pip install Flask-mail
+ pip install "tablib[all]"
